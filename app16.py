c2a8adbb3b7fa9310f702f6fd0a9e887c7b4088a
youn <100547827+jiyoun29@users.noreply.github.com>
2024-10-31 10:16:57 +0900

    Update app16.py

--- app16.py
+++ app16.py
@@ -6,6 +6,9 @@ st.set_page_config(layout="wide")
 # 제목 설정
 st.title("비디오 사물 검출 앱")
 
+# 파일 업로드 버튼을 상단으로 이동
+uploaded_file = st.file_uploader("비디오 파일을 업로드하세요", type=["mp4", "mov", "avi"])
+
 # 전체 레이아웃을 컨테이너로 감싸기
 with st.container(): # 이 with절이란? 하나의 기능을 하는 코드를 묶어주는 것(가독성 높이기)
     col1, col2 = st.columns(2)  # 열을 균등하게 분배하여 넓게 표시
